1e4a91e2e56702d1bae38dba2db6382566933aba
unknown <ouchida2badre@gmail.com>
2021-05-06 22:34:05 +0200

    update procfile42

--- RealEstate/RealEstate/settings.py
+++ RealEstate/RealEstate/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/css')
 
 
 # rest framework 
